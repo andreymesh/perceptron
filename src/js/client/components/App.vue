6c6998460aa898d049ec396bbec088510ebb3f60
Lennoximus <lennoximus@gmail.com>
2017-11-05 22:04:31 +0300

    Initial commit

---
+++ src/js/client/components/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import Canvas from './common/Canvas.vue'
+  import Controls from './common/Controls.vue'
+
+  export default {
+    data () {
+      return {
+        learningRate: null,
+        weights: [],
+        cells: []
+      }
+    },
+    components: {
+      'c-canvas': Canvas,
+      'c-controls': Controls
+    }
+  }
+</script>
+
+<template>
+  <div class="g-wrapper">
+    <c-canvas/>
+    <c-controls/>
+  </div>
+</template>
---
+++ src/js/client/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import store from './store'
+import router from './router'
+
+Vue.config.performance = true
+
+new Vue({ // eslint-disable-line
+  el: '#app',
+  store,
+  router,
+  render: h => h('router-view')
+})
